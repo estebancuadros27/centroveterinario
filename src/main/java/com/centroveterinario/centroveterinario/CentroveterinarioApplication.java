7a6ba2890d4b231ca309b5b2f5f46eff810c0495
Esteban Cuadros <estebancuadros@MacBook-Pro-de-Esteban.local>
2025-11-21 18:54:33 -0500

    Estructura proyecto maven

---
+++ src/main/java/com/centroveterinario/centroveterinario/CentroveterinarioApplication.java
@@ -0,0 +1,13 @@
+package com.centroveterinario.centroveterinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroveterinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroveterinarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centroveterinario/centroveterinario/CentroveterinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroveterinario.centroveterinario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroveterinarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
